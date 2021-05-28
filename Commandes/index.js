e094e71eb1a54ac429920bf7e33b1d2671526ff3
Renard Nocturne <yanicehgpro@yahoo.com>
2021-05-28 23:33:39 +0200

    feat: :zap: Création de la commande "help" et optimisation du code !

--- Commandes/index.js
+++ Commandes/index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { token, prefix } = require('../Ignorer/config.js');
+const { MessageEmbed } = require('discord.js');
 bot = new Discord.Client
 
 config = require('../Ignorer/config.json'),
@@ -9,39 +10,34 @@ bot.login(`${token}`)
 
 bot.on('ready', () => {
     console.log("Bot successfully logged in !")
+
+    bot.user.setPresence({ activity: { name: `l'alpha de CodingHelp.`, type: 'WATCHING' }, status: 'online'})
+      .then(console.log)
+      .catch(console.error);
 });
 
 bot.on('message', message => {
     if (message.type !== 'DEFAULT' || message.author.bot) return;
-})
 
-bot.on ('message', message => {
-    if (message.content.startsWith(`${prefix}Bonjour`)) {
-        message.channel.send("Hey BG !");
-    };
-});
+    const args = message.content.slice(prefix.length).split(/ +/);
 
+    const command = args.shift().toLowerCase();
 
-bot.on('message', message => {
-    if (message.content === 'help'){
-    message.reply('le bot est en **dévellopement** si tu veux faire une suggestion dm nat ou renardNocturne ^^')
-  }
-})
-
-bot.on('message', message => {
-    // If the message is "how to embed"
-    if (message.content === 'how to embed') {
-      // We can create embeds using the MessageEmbed constructor
-      // Read more about all that you can do with the constructor
-      // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    function embedMaker (title = "Titre", description = "Quelque chose semble causer problème :thinking:", footer = `Demandée par ${message.author.username}`, color = "5D6C9D") {
       const embed = new MessageEmbed()
-        // Set the title of the field
-        .setTitle('A slick little embed')
-        // Set the color of the embed
-        .setColor(0xff0000)
-        // Set the main content of the embed
-        .setDescription('Hello, this is a slick embed!');
-      // Send the embed to the same channel as the message
-      message.channel.send(embed);
-    }
-  });
+        .setTitle(title)
+        .setColor(color)
+        .setDescription(description)
+        .setFooter(footer)
+        .setTimestamp();
+      return embed
+    };
+    
+    if (command === "bonjour") {
+      message.channel.send("Hey BG !");
+    };
+
+    if (command === "help") {
+      message.channel.send(embedMaker(":warning: Le bot est en cours de développement ! :warning:", `**Je suis actuellement en Alpha** la seule commande actuellement disponible est **${prefix}suggest** afin d'envoyer vos suggestions à Nat et RenardNocturne ! **Si vous repérez un bug/dysfonctionnement** n'hésitez pas non plus à utiliser cette commande !`))
+    };
+})
