d24cba152eca97fa07831ddebe4fd8e8afbe9d73
Renard Nocturne <yanicehgpro@yahoo.com>
2021-05-28 21:19:50 +0200

    feat: :zap: Le prefix marche !

--- Commandes/index.js
+++ Commandes/index.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js'),
-const { token, prefix } = require('../Ignorer/config.js'),
+const Discord = require('discord.js');
+const { token, prefix } = require('../Ignorer/config.js');
 bot = new Discord.Client
 
 config = require('../Ignorer/config.json'),
@@ -16,7 +16,7 @@ bot.on('message', message => {
 })
 
 bot.on ('message', message => {
-    if (message.content.startsWith(`${prefix} Bonjour`)) {
+    if (message.content.startsWith(`${prefix}Bonjour`)) {
         message.channel.send("Hey BG !");
     };
 });
@@ -24,6 +24,24 @@ bot.on ('message', message => {
 
 bot.on('message', message => {
     if (message.content === 'help'){
-    message.reply('le bot est en **dévellopement** si tu veux faire une suggestion dm nat ou renardNocturne ^^ ')
+    message.reply('le bot est en **dévellopement** si tu veux faire une suggestion dm nat ou renardNocturne ^^')
   }
 })
+
+bot.on('message', message => {
+    // If the message is "how to embed"
+    if (message.content === 'how to embed') {
+      // We can create embeds using the MessageEmbed constructor
+      // Read more about all that you can do with the constructor
+      // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+      const embed = new MessageEmbed()
+        // Set the title of the field
+        .setTitle('A slick little embed')
+        // Set the color of the embed
+        .setColor(0xff0000)
+        // Set the main content of the embed
+        .setDescription('Hello, this is a slick embed!');
+      // Send the embed to the same channel as the message
+      message.channel.send(embed);
+    }
+  });
