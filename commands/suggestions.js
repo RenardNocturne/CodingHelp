61dee532412df060db37892e352a497f1b56aec7
Renard Nocturne <yanicehgpro@yahoo.com>
2021-05-29 13:22:29 +0200

    feat: :zap: Ajout de la commande >_suggestions !

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Envoies ta suggestion !',
 
     execute(message, args, embedMaker) {
-        const channel = message.guild.channels.cache.find(c => c.id === suggestionsChannelID);
+        const channel = message.guild.channels.cache.find(c => c.id === 848156324248420402);
         if(!channel) return message.channel.send(embedMaker('Une erreur est survenue !', 'Le salon de suggestions n\'a pas été trouvé !', `Demandée par ${message.author.username}`, 'DE2916'));
         if(args[0] === undefined) return message.channel.send(embedMaker('Une erreur est survenue !', 'Aucune suggestion apportée !', `Demandée par ${message.author.username}`, 'DE2916I'))
 
