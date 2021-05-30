42dd3352446f8f792a889b51fac543151fa57232
nat <capitaine.nat54230@gmail.com>
2021-05-30 14:32:19 +0200

    changement

---
+++ OneDrive/Bureau/BotDiscord -CodingTtime-/commands/Miscellaneous/annonces.js
@@ -0,0 +1,16 @@
+module.exports.run = (bot, message, args, embedMaker, prefix, embedError) => {
+
+
+
+
+
+
+}
+
+
+
+
+module.exports.help = {
+    name: 'ping-annonces',
+    description: 'affiche les commandes du bot',
+  }
--- OneDrive/Bureau/BotDiscord -CodingTtime-/commands/Miscellaneous/commands.js
+++ OneDrive/Bureau/BotDiscord -CodingTtime-/commands/Miscellaneous/commands.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args, embedMaker, prefix, embedError) => {
 
     const embed = new MessageEmbed()
     .setTitle('La liste des commandes actuelles')
-    .setDescription("*___les commandes activent sont :___*\n> ** >_description**\n> ** >_help**\n> ** >_suggestions**\n> ** >_avatar**\n> ** >_nettoyage**\n> ** >_ping**\n> ** >_server**\n*____Les commandes non-activent sont :____*\n> ** >_play**")
+    .setDescription("*___les commandes activent sont :___*\n> ** >_description**\n> ** >_help**\n> ** >_suggestions**\n> ** >_avatar**\n> ** >_nettoyage**\n> ** >_ping**\n> ** >_server**\n*____Les commandes non-activent sont :____*\n> ** >_play**\n> ** >_report**")
     .setFooter(`Demandée par ${message.author.username}`, `${message.author.avatarURL()}`)
     .setColor("5D6C9D")
     .setThumbnail('attachment://PP.jpg')
