576efed51d3800a34791635bfcf3853d07bebbe2
Renard Nocturne <yanicehgpro@yahoo.com>
2021-06-02 14:14:51 +0200

    fix: :zap: Catch de l'erreur sur la commande >_report !

--- commands/Miscellaneous/report.js
+++ commands/Miscellaneous/report.js
@@ -2,8 +2,10 @@ module.exports.run = (bot, message, args, embedMaker, prefix, embedError) => {
 
     let messageArgs = args.join(' ');
 
-    bot.channels.cache.get('849347521352695868').send(embedMaker("Nouveau report en vue !", messageArgs));
-    message.channel.send(embedMaker("Report envoyé !", `${message.author.username}, votre report a bien été envoyé ! Merci de nous faire évoluer !`))
+    bot.channels.cache.get('849610360319508490').send(embedMaker("Nouveau report en vue !", messageArgs));
+
+    message.author.send((embedMaker("Report envoyé !", `${message.author.username}, votre report a bien été envoyé ! Merci de nous faire évoluer !\n \n \`${messageArgs}\``)))
+    .catch(() => message.channel.send(embedError(undefined, `${message.author.username}, il semblerait que vous soyez fermés au messages privés ! **Votre message à tout de même été envoyé !**`)));
 }
 module.exports.help = {
 name: "report",
