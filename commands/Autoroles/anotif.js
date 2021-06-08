9a6a2ffb5cc05a6f7c8bf37164820509180f2d9f
Renard Nocturne <yanicehgpro@yahoo.com>
2021-06-08 17:08:53 +0200

    fix: :bug: Refonte des commandes d'autorôle !

--- commands/Autoroles/anotif.js
+++ commands/Autoroles/anotif.js
@@ -3,7 +3,8 @@ module.exports.run = (bot, message, args, embedMaker, prefix, embedError) => {
     let annonceRole = '849313732785733732';
 
     if (message.member.roles.cache.has(annonceRole)) {
-        message.channel.send(embedError(undefined, `${message.author.username} le rôle ${message.member.roles.cache.get(annonceRole)} vous a déjà été attribué !`)).then(msg => msg.delete({timeout: 5000}))
+        message.member.roles.remove(annonceRole)
+        message.channel.send(embedMaker("Rôle retiré !", `${message.author.username}, vous venez de perdre le rôle ${message.member.guild.roles.cache.get(annonceRole)}`)).then(msg => msg.delete({timeout: 5000}))
     } else {
     message.channel.send(embedMaker("Rôle accordé !", `${message.author.username}, vous venez de recevoir le rôle ${message.member.guild.roles.cache.get(annonceRole)}`)).then(msg => msg.delete({timeout: 5000}))
     message.member.roles.add(annonceRole)
@@ -12,7 +13,7 @@ module.exports.run = (bot, message, args, embedMaker, prefix, embedError) => {
 
 module.exports.help = {
 name: 'anotif',
-description: 'Vous ajoute le rôle de notifications d\'annonces.',
+description: 'Vous ajoute/retire le rôle de notifications d\'annonces.',
 args: false,
 usage: "",
 cooldown: 15,
